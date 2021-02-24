960c04983f893b1e9ceb96d75d3b694a08d9266c
isienmai <ben.strutt@hotmail.co.uk>
2021-02-24 14:52:13 +0000

    apparently not

--- FloodLevels.php
+++ FloodLevels.php
@@ -22,9 +22,11 @@ function GetMonitoringStationRiverLevel($stationId)
 
 	$response = file_get_contents($apiAccessUrl);
 
+	echo "Raw Response" . $response;
+
 	$decodedResponse = json_decode($response, true, JSON_PRETTY_PRINT);
 	echo $decodedResponse;
-	echo "Items:" $decodedResponse['items'];
+	//echo "Items:" $decodedResponse['items'];
 	/*echo "Station Reference:" . $decodedResponse['items'][0]['stationReference'];
 	echo "Water Level:" . $decodedResponse['items'][0]['latestReading']['value'];*/
 	return 3;
