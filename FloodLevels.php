30e487c0b5cf7ae4dfa074170abed679878b0179
isienmai <ben.strutt@hotmail.co.uk>
2021-02-24 15:09:46 +0000

    Good Lord the stats are looking fly

--- FloodLevels.php
+++ FloodLevels.php
@@ -21,11 +21,9 @@ function GetMonitoringStationRiverLevel($stationId)
 	$apiAccessUrl = $root . $measuresExtension;
 
 	$response = file_get_contents($apiAccessUrl);
-
-	echo "Raw Response" . $response;
-
 	$decodedResponse = json_decode($response, true);
-	echo "<p>Water Level:" . $decodedResponse['items'][0]['latestReading']['value'] . "</p>";
-	return 3;
+
+	$riverLevel = $decodedResponse['items'][0]['latestReading']['value'];
+	return intval($riverLevel);
 }
 ?>
