6c1dde353ec6f5b41dc128b86c2a6c525298b95c
isienmai <ben.strutt@hotmail.co.uk>
2021-02-24 15:49:22 +0000

    ok so it seems I can't access global variables frim within a function wihtout being explicit

--- FloodLevels.php
+++ FloodLevels.php
@@ -1,8 +1,8 @@
 <?php
 // Flooding levels chosen by adding 1m to the "minor flooding is possible" level
 // see for those numbers https://flood-warning-information.service.gov.uk/station/2092 and https://flood-warning-information.service.gov.uk/station/2039
-$barbourneFloodingLevel = floatval(4.35);
-$diglisFloodingLevel = floatval(3.80);
+$barbourneFloodingLevel = 4.35;
+$diglisFloodingLevel = 3.80;
 
 function IsWorcesterUnderwater()
 {
@@ -12,7 +12,7 @@ function IsWorcesterUnderwater()
 function IsBarbourneFlooding()
 {
 	$currentLevel = GetBarbourneRiverLevel();
-	$isFlooding = $currentLevel >= $barbourneFloodingLevel;
+	$isFlooding = $currentLevel >= $$GLOBALS['barbourneFloodingLevel'];
 	echo "CURRENT LEVEL:" . strval($currentLevel);
 	echo "FLOOD LEVEL:" . strval($barbourneFloodingLevel);
 	echo "IS FLOODING: " . strval($isFlooding);
