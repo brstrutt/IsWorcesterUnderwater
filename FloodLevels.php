823b189acfeba01e0fe8efa446336119ee5de37a
isienmai <ben.strutt@hotmail.co.uk>
2021-02-24 12:34:07 +0000

    Maybe good?

--- FloodLevels.php
+++ FloodLevels.php
@@ -33,23 +33,18 @@ function GetMonitoringStationRiverLevel($stationId)
 
 	$response = curl_exec($curl);
 	$err = curl_error($curl);
-	
+
 	curl_close($curl);
+
+	if ($err)
+	{
+		echo "CURL Error #:" . $err;
+	}
+	else
+	{
+		echo $response;
+	}
 
 	return 3;
 }
-/*
-	/*
-
-
-
-
-	curl_close($curl);
-
-	if ($err) {
-		echo "cURL Error #:" . $err;
-	} else {
-		echo $response;
-	}
-}*/
 ?>
