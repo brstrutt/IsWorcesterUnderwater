f9b18c6a8af42532dcc6341dd32a769b373c42b4
isienmai <ben.strutt@hotmail.co.uk>
2021-02-24 13:24:31 +0000

    Try printing the api url to help with debugging

--- FloodLevels.php
+++ FloodLevels.php
@@ -18,14 +18,16 @@ function GetMonitoringStationRiverLevel($stationId)
 {
 	$root = "http://environment.data.gov.uk/flood-monitoring";
 	$measuresExtension = "/id/stations/" . strval(stationId) . "/measures";
+	$apiAccessUrl = $root . $measuresExtension;
 
-	$response = file_get_contents($root . $measuresExtension);
+	$response = file_get_contents($apiAccessUrl);
 	/*$curl = curl_init($root . $measuresExtension);
 
 	$response = curl_exec($curl);
 	$err = curl_error($curl);
 
 	curl_close($curl);*/
+	echo "URL: " . $apiAccessUrl;
 	echo "Results:" . $response;
 	return 3;
 }
