482d43e1741341926c5d4de82550de1b0ae19b4b
isienmai <ben.strutt@hotmail.co.uk>
2021-02-24 14:33:23 +0000

    Is it the json decode?

--- FloodLevels.php
+++ FloodLevels.php
@@ -23,9 +23,10 @@ function GetMonitoringStationRiverLevel($stationId)
 	$response = file_get_contents($apiAccessUrl);
 
 	$decodedResponse = json_decode($response);
-	echo "Station Reference:" . $decodedResponse['items'][0]['stationReference'];
-	echo "Water Level:" . $decodedResponse['items'][0]['latestReading']['value'];
-
+	echo $decodedResponse;
+	//echo "Items:" $decodedResponse['items'];
+	//echo "Station Reference:" . $decodedResponse['items'][0]['stationReference'];
+	//echo "Water Level:" . $decodedResponse['items'][0]['latestReading']['value'];
 	return 3;
 }
 ?>
