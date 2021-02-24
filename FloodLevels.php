f8d61704cb23279e37a529f71cae77ef9739b866
isienmai <ben.strutt@hotmail.co.uk>
2021-02-24 12:30:59 +0000

    curly boi

--- FloodLevels.php
+++ FloodLevels.php
@@ -18,11 +18,13 @@ function GetMonitoringStationRiverLevel($stationId)
 {
 	$root = "http://environment.data.gov.uk/flood-monitoring";
 	$measuresExtension = "/id/stations/" . strval(stationId) . "/measures";
+
+	$curl = curl_init();
+	
 	return 3;
 }
 /*
-	retun 3;
-	/*$curl = curl_init();
+	/*
 
 	curl_setopt_array($curl, [
 		CURLOPT_URL => $root . $measuresExtension,
