639c5cc592fdeb1213d4e293196291be50b6eec0
isienmai <ben.strutt@hotmail.co.uk>
2021-02-24 12:41:34 +0000

    Do it work better this way?

--- FloodLevels.php
+++ FloodLevels.php
@@ -19,23 +19,12 @@ function GetMonitoringStationRiverLevel($stationId)
 	$root = "http://environment.data.gov.uk/flood-monitoring";
 	$measuresExtension = "/id/stations/" . strval(stationId) . "/measures";
 
-	$curl = curl_init();
-	curl_setopt_array($curl, [
-		CURLOPT_URL => $root . $measuresExtension,
-		CURLOPT_RETURNTRANSFER => true,
-		CURLOPT_FOLLOWLOCATION => true,
-		CURLOPT_ENCODING => "",
-		CURLOPT_MAXREDIRS => 10,
-		CURLOPT_TIMEOUT => 30,
-		CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-		CURLOPT_CUSTOMREQUEST => "POST",
-	]);
-
+	$curl = curl_init($root . $measuresExtension);
 	$response = curl_exec($curl);
 	$err = curl_error($curl);
 
 	curl_close($curl);
-	
+
 	if ($err)
 	{
 		echo "CURL Error #:" . $err;
