6392acb420a0158e441100986df43b1709a1479b
isienmai <ben.strutt@hotmail.co.uk>
2021-02-24 12:17:45 +0000

    LETS TRY CURLING THE DOODEEDOODAA

--- FloodLevels.php
+++ FloodLevels.php
@@ -1,6 +1,45 @@
 <?php
 function IsWorcesterUnderwater()
 {
-  return False;
+	return False;
+}
+
+function GetBarbourneRiverLevel()
+{
+	return GetMonitoringStationRiverLevel(2092);
+}
+
+function GetDiglisRiverlLevel()
+{
+	return GetMonitoringStationRiverLevel(2039);
+}
+
+function GetMonitoringStationRiverLevel(stationId)
+{
+	$root = "http://environment.data.gov.uk/flood-monitoring";
+	$measuresExtension = "/id/stations/" . strval(stationId) . "/measures";
+	$curl = curl_init();
+
+	curl_setopt_array($curl, [
+		CURLOPT_URL => $root . $measuresExtension,
+		CURLOPT_RETURNTRANSFER => true,
+		CURLOPT_FOLLOWLOCATION => true,
+		CURLOPT_ENCODING => "",
+		CURLOPT_MAXREDIRS => 10,
+		CURLOPT_TIMEOUT => 30,
+		CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+		CURLOPT_CUSTOMREQUEST => "POST",
+	]);
+
+	$response = curl_exec($curl);
+	$err = curl_error($curl);
+
+	curl_close($curl);
+
+	if ($err) {
+		echo "cURL Error #:" . $err;
+	} else {
+		echo $response;
+	}
 }
 ?>
