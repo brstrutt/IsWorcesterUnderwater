1526efd5aff536587edf7260782e2c2f5fd40bf7
isienmai <ben.strutt@hotmail.co.uk>
2021-02-24 12:24:39 +0000

    FINE ILL JUST DELET

--- FloodLevels.php
+++ FloodLevels.php
@@ -6,19 +6,6 @@ function IsWorcesterUnderwater()
 
 function GetBarbourneRiverLevel()
 {
-	return GetMonitoringStationRiverLevel(2092);
-}
-
-function GetDiglisRiverlLevel()
-{
-	return GetMonitoringStationRiverLevel(2039);
-}
-
-function GetMonitoringStationRiverLevel(stationId)
-{
-	//$root = "http://environment.data.gov.uk/flood-monitoring";
-	//$measuresExtension = "/id/stations/" . strval(stationId) . "/measures";
-
 	return 3;
 }
 ?>
