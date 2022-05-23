86f5bb446669a67ab454e753be518eaaf65db3be
isienmai <ben.strutt@hotmail.co.uk>
2022-05-23 18:02:46 +0100

    Added some noise to the waves to make them a little less uniform.

--- WaveAnimation.php
+++ WaveAnimation.php
@@ -56,30 +56,40 @@ class key_frame
     {
         return count($this->points);
     }
+
+    function ApplyRandomOffsetToEachPoint()
+    {
+        for($i = 0; $i < $this->Size(); $i++)
+        {
+            $this->points[$i]->y += (rand(0, 100) - 50) / 500;
+        }
+    }
 }
 
 // Waves need to be on a line that is at an angle. So We gotta interpolate a bunch of 
 // heights and then generate 4 sets of keyframes to use for the animation
 function GenerateWaveKeyframeHeights($leftHeight, $rightHeight)
 {
-    $keyFrames = GenerateKeyframes($leftHeight, $rightHeight, 1);
+    $keyFrames = GenerateKeyframes($leftHeight, $rightHeight, 0.5);
 
     EchoKeyframesAsCssVariables($keyFrames);
 }
 
 function GenerateKeyframes($leftHeight, $rightHeight, $waveHeight)
 {
-    $numOfPoints = 40;
+    $numOfPoints = 20;
     $coreWave = GenerateWave($leftHeight, $rightHeight, $waveHeight, $numOfPoints);
 
     $translationBetweenFrames = new coord(100/($numOfPoints - 1), ($rightHeight - $leftHeight) / ($numOfPoints - 1));
 
     $keyFrames = array();
-    for($i = 0; $i < $numOfPoints; $i++)
+    $numOfFrames = 4;
+    for($i = 0; $i < $numOfFrames; $i++)
     {
         $newKeyFrame = clone $coreWave;
         $newKeyFrame = MoveKeyframePoints($newKeyFrame, $translationBetweenFrames->multiply($i * -1));
         $newKeyFrame = CycleKeyframePoints($newKeyFrame, $translationBetweenFrames);
+        $newKeyFrame->ApplyRandomOffsetToEachPoint();
         array_push($keyFrames, $newKeyFrame);
     }
     return $keyFrames;
